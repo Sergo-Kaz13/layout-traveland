1c9c82ef4348cacbcf9845095a0b0d434938e499
Sergo_Kaz <wwe619dx@ukr.net>
2023-10-28 23:20:28 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -17,6 +17,36 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    850: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 40,
+    },
+    1600: {
+      slidesPerView: 5,
+      spaceBetween: 73,
+    },
+  },
+
   // And if we need scrollbar
   // scrollbar: {
   //   el: ".swiper-scrollbar",
